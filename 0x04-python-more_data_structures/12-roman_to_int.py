5ec783ef6a1fd883aa6b206514377c151b0d5dca
Simonchristopher404 <simonchristopher404@gmail.com>
2024-01-10 04:58:45 -0800

    alx task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,33 +1,60 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
-        return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
-    rep = 0
 
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
+def value(roman):
+    """returns value of each Roman symbol
+    """
+    if roman == 'I':
+        return 1
+    if roman == 'V':
+        return 5
+    if roman == 'X':
+        return 10
+    if roman == 'L':
+        return 50
+    if roman == 'C':
+        return 100
+    if roman == 'D':
+        return 500
+    if roman == 'M':
+        return 1000
+    return 0
+
+
+def roman_to_int(roman_string):
+    """converts a Roman numeral to an integer
+    You can assume the number will be between 1 to 3999.
+    def roman_to_int(roman_string) must return an integer
+    If the roman_string is not a string or None, return 0
+    """
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    result = 0
+    i = 0
+
+    while i < len(roman_string):
+        # getting value of symbol roman_string[i]
+        str1 = value(roman_string[i])
+
+        if i + 1 < len(roman_string):
+            # getting value of symbol roman_string[i + 1]
+            str2 = value(roman_string[i + 1])
+
+            # comparing both values
+            if str1 >= str2:
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                result = result + str1
+                i = i + 1
+            else:
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                result = result + str2 - str1
+                i = i + 2
         else:
-            rep -= numbers[i]
-
-    return rep
-
-
-Advance task
-vi 100-weight_average.py
-
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
-
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
+            result = result + str1
+            i = i + 1
+    return result
