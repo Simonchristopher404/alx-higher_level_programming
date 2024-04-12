5f2d324fbd0fd6f758e625b85e1e8835f4dc9a75
Simon <simonchristopher404@gmail.com>
2024-04-12 03:01:09 -0700

    0x0F-python-object_relational_mapping

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+let list = require('./100-data').list;
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(list.map((element, index) => element * index));
